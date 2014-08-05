8ddd9705bbb50302f470afe52f49bd9f948615ef
KVNKWN <kvnkwn88@gmail.com>
2014-08-05 15:54:14 -0400

    Make a basic User model (including secure passwords)

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,11 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+# def full_title(page_title)
+#   base_title = "Ruby on Rails Tutorial Sample App"
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+#   end
+# end
+# This code was replaced by tests in spec/helpers/application_helper_spec.rb that reference application_help.rb where full_title is defined
